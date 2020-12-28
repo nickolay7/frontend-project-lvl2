4744820048aff4a4c52d9516e98ab229de56b8f8
Nickolay Semutkin <shkelal50@gmail.com>
2020-12-28 22:14:37 +0300

    aded action() to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,13 @@
 #!/bin/env node
 
 import { program } from 'commander';
+import jsonDiff from '../jsonDiff.js';
 
 program.description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --vers', 'output the version number')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .parse();
+  .action(function (filepath1, filepath2) {
+    jsonDiff(filepath1, filepath2);
+  })
+  .parse(process.argv);

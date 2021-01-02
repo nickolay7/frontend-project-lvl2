0e2a48d2d5d69ad8cd68e4876d9dded07d53eddd
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-02 22:18:56 +0300

    fefactor jsonDiff.js

--- jsonDiff.js
+++ jsonDiff.js
@@ -1,23 +1,29 @@
 import _ from 'lodash';
-import fs from "fs";
+import fs from 'fs';
+// import path from 'path';
+
+const readFile = (path) => {
+  // const fullPath = path.resolve(path, process.cwd());
+  return JSON.parse(fs.readFileSync(path, 'utf-8'));
+}
 
 export default (filepath1, filepath2) => {
-  const obj1 = JSON.parse(fs.readFileSync(filepath1, 'utf-8'));
-  const obj2 = JSON.parse(fs.readFileSync(filepath2, 'utf-8'));
+  const obj1 = readFile(filepath1);
+  const obj2 = readFile(filepath2);
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const allKeys = _.union(keys1, keys2);
   const res = `{\n${allKeys.map((key) => {
     if (!(_.has(obj1, key)) && (_.has(obj2, key))) {
-      return ` + ${key}: ${obj2[key]}`;
+      return `  + ${key}: ${obj2[key]}`;
     }
     if ((_.has(obj1, key)) && !(_.has(obj2, key))) {
-      return ` - ${key}: ${obj1[key]}`;
+      return `  - ${key}: ${obj1[key]}`;
     }
     if (obj1[key] === obj2[key]) {
-      return `   ${key}: ${obj2[key]}`;
+      return `    ${key}: ${obj2[key]}`;
     }
-    return ` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`;
+    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
   }).sort(function(a, b) {
     if (a[3] > b[3]) {
       return 1; }

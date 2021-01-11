1c2e50724d859cd3111d727768f7f1dfe2156c52
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-12 00:12:28 +0300

    refactor gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,10 +2,11 @@
 
 import program from 'commander';
 import jsonDiff from '../index.js';
+import stylish from '../stylish.js';
 
 program.description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --vers', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(jsonDiff(filepath1, filepath2)))
+  .action((filepath1, filepath2) => console.log(stylish(jsonDiff(filepath1, filepath2))))
   .parse(process.argv);

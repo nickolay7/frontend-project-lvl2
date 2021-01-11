dd7e28652e1df2781b509eddd8eef184295cd208
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-11 22:08:25 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 import readFile from '../src/parsers.js';
+import formatter from '../src/stylish.js'
 
 const expectedData = { nested: '', plain: '' };
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -16,26 +17,26 @@ test('genDiff.json', () => {
   const path1 = getFixturePath('file.json');
   const path2 = getFixturePath('anotherFile.json');
   const expected = expectedData.plain;
-  expect(genDiff(path1, path2)).toEqual(expected);
+  expect(formatter(genDiff(path1, path2))).toEqual(expected);
 });
 
 test('genDiff.yaml', () => {
   const path1 = getFixturePath('file.yaml');
   const path2 = getFixturePath('anotherFile.yaml');
   const expected = expectedData.plain;
-  expect(genDiff(path1, path2)).toEqual(expected);
+  expect(formatter(genDiff(path1, path2))).toEqual(expected);
 });
 
 test('genDiff.json nested', () => {
   const path1 = getFixturePath('nestedFile.json');
   const path2 = getFixturePath('anotherNestedFile.json');
   const expected = expectedData.nested;
-  expect(genDiff(path1, path2)).toEqual(expected);
+  expect(formatter(genDiff(path1, path2))).toEqual(expected);
 });
 
 test('genDiff.yaml nested', () => {
   const path1 = getFixturePath('nestedFile.yaml');
   const path2 = getFixturePath('anotherNestedFile.yaml');
   const expected = expectedData.nested;
-  expect(genDiff(path1, path2)).toEqual(expected);
+  expect(formatter(genDiff(path1, path2))).toEqual(expected);
 });

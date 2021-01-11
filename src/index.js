63cb959b26ca9a115194a2e7df6755e1e4fba0bb
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-11 21:53:17 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import readFile from './parsers.js';
-import formatter from './stylish.js';
 
 export default (filepath1, filepath2) => {
   const data1 = readFile(filepath1);
@@ -38,35 +37,5 @@ export default (filepath1, filepath2) => {
     });
     return handler;
   };
-  // const handler = allKeys.map((key) => {
-  //   if (!(_.has(data1, key)) && (_.has(data2, key))) {
-  //     return `  + ${key}: ${data2[key]}`;
-  //   }
-  //   if ((_.has(data1, key)) && !(_.has(data2, key))) {
-  //     return `  - ${key}: ${data1[key]}`;
-  //   }
-  //   if (data1[key] === data2[key]) {
-  //     return `    ${key}: ${data2[key]}`;
-  //   }
-  //   return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-  // }).sort((a, b) => {
-  //   if (a[4] > b[4]) {
-  //     return 1;
-  //   }
-  //   if (a[4] < b[4]) {
-  //     return -1;
-  //   }
-  //   return 0;
-  // }).join('\n');
-  // return `{\n${handler}\n}`;
-  // return formatter(handler.sort((a, b) => {
-  //   if (a.key > b.key) {
-  //     return 1;
-  //   }
-  //   if (a.key < b.key) {
-  //     return -1;
-  //   }
-  //   return 0;
-  // }));
-  return formatter(getDiff(data1, data2));
+  return getDiff(data1, data2);
 };

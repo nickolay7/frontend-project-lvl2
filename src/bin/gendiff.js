f1014ff49596d33768702af694cb38cf051c2c75
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-12 23:07:13 +0300

    refactor gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,5 +8,11 @@ program.description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --vers', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(stylish(jsonDiff(filepath1, filepath2))))
+  .action((filepath1, filepath2) => {
+    if (program.format === 'stylish') {
+      console.log(stylish(jsonDiff(filepath1, filepath2)));
+    } else {
+      console.log(program.format);
+    }
+  })
   .parse(process.argv);

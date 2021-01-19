642a1fd79f7f871b52c439111e5876b37ba0d377
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-19 23:26:39 +0300

    refactor

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -2,9 +2,9 @@ const json = (data) => {
   if (typeof data !== 'object' || data === null) {
     return data;
   }
-return !Array.isArray(data)
-  ? Object.entries(data).reduce((acc, [key, value]) => ({ ...acc, [key]: json(value) }), {})
-  : data.reduce((acc, { sign, key, value }) => {
+  return !Array.isArray(data)
+    ? Object.entries(data).reduce((acc, [key, value]) => ({ ...acc, [key]: json(value) }), {})
+    : data.reduce((acc, { sign, key, value }) => {
       if (sign === '-' || (sign === ' ' && typeof value !== 'object')) {
         return acc;
       }

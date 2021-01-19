63558f58d298b1da82bb128d3b2625289b2674f4
Nickolay Semutkin <shkelal50@gmail.com>
2021-01-19 09:01:07 +0300

    refactor

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,4 +1,13 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './json.js';
 
-export default (data, formatName) => (formatName === 'stylish' ? stylish(data) : plain(data));
+export default (data, formatName) => {
+  if (formatName === 'stylish') {
+    return stylish(data);
+  }
+  if (formatName === 'plain') {
+    return plain(data);
+  }
+  return json(data);
+};

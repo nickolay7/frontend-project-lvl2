708d05b14088cc868803ed3989d598f38d397946
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-14 19:16:20 +0300

    simplified sorting :)

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ import fs from 'fs';
 import parse from './parsers.js';
 import format from './formatters/index.js';
 
-const cond = (a, b) => {
-  if (a.key > b.key) {
-    return 1;
-  }
-  if (a.key < b.key) {
-    return -1;
-  }
-  return 0;
-};
 export default (filepath1, filepath2, formatName = 'stylish') => {
   const getDiffFiles = (data1, data2) => {
     const allKeys = _.union(Object.keys(data1), Object.keys(data2));
-    return allKeys.flatMap((key) => {
+    const sortedKeys = _.sortBy(allKeys);
+    return sortedKeys.flatMap((key) => {
       const val1 = data1[key];
       const val2 = data2[key];
       if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
@@ -33,7 +25,7 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
         return { type: 'unchanged', key, value: val1 };
       }
       return [{ type: 'update', key, value: val1 }, { type: 'updated', key, value: val2 }];
-    }).sort(cond);
+    });
   };
   const file1 = fs.readFileSync(filepath1, 'utf-8');
   const file2 = fs.readFileSync(filepath2, 'utf-8');

3ea9b5aedb9d87c36c75bf6abcfa9f746c853889
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-14 18:46:00 +0300

    renamed function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import program from 'commander';
-import jsonDiff from '../src/index.js';
+import getDiff from '../src/index.js';
 
 program.description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --vers', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(jsonDiff(filepath1, filepath2, program.format));
+    console.log(getDiff(filepath1, filepath2, program.format));
   })
   .parse(process.argv);

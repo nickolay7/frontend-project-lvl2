6d156e9ce3f918eb592728b426a462517424c98f
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-16 00:41:24 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import format from './formatters/index.js';
 import getDiffFiles from './getDiffFiles.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const file1 = fs.readFileSync(filepath1, 'utf-8');
-  const file2 = fs.readFileSync(filepath2, 'utf-8');
-  const diff = getDiffFiles(parse(filepath1, file1), parse(filepath2, file2));
+  const data1 = fs.readFileSync(filepath1, 'utf-8');
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
+  const diff = getDiffFiles(parse(filepath1, data1), parse(filepath2, data2));
   return format(diff, formatName);
 };

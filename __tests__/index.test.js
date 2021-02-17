0b94896a749b5ad500cc7c21ee066ff7e6dfea4f
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-18 00:31:57 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,23 +3,21 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const expectedData = {};
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getPath = (fileName) => getFixturePath(fileName);
-
-expectedData.plain = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
-expectedData.nested = fs.readFileSync(getFixturePath('nested.txt'), 'utf-8');
-expectedData.json = fs.readFileSync(getFixturePath('json.txt'), 'utf-8');
+const expectedPlain = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
+const expectedNested = fs.readFileSync(getFixturePath('nested.txt'), 'utf-8');
+const expectedJson = fs.readFileSync(getFixturePath('json.txt'), 'utf-8');
 
 const cases = [
-  [getPath('nestedFile.json'), getPath('anotherNestedFile.json'), 'stylish', expectedData.nested],
-  [getPath('nestedFile.yaml'), getPath('anotherNestedFile.yaml'), 'stylish', expectedData.nested],
-  [getPath('file.ini'), getPath('anotherFile.ini'), 'stylish', expectedData.nested],
-  [getPath('nestedFile.json'), getPath('anotherNestedFile.json'), 'plain', expectedData.plain],
-  [getPath('nestedFile.yaml'), getPath('anotherNestedFile.yaml'), 'plain', expectedData.plain],
-  [getPath('file.ini'), getPath('anotherFile.ini'), 'plain', expectedData.plain],
-  [getPath('nestedFile.json'), getPath('anotherNestedFile.json'), 'json', expectedData.json],
+  [getPath('nestedFile.json'), getPath('anotherNestedFile.json'), 'stylish', expectedNested],
+  [getPath('nestedFile.yaml'), getPath('anotherNestedFile.yaml'), 'stylish', expectedNested],
+  [getPath('file.ini'), getPath('anotherFile.ini'), 'stylish', expectedNested],
+  [getPath('nestedFile.json'), getPath('anotherNestedFile.json'), 'plain', expectedPlain],
+  [getPath('nestedFile.yaml'), getPath('anotherNestedFile.yaml'), 'plain', expectedPlain],
+  [getPath('file.ini'), getPath('anotherFile.ini'), 'plain', expectedPlain],
+  [getPath('nestedFile.json'), getPath('anotherNestedFile.json'), 'json', expectedJson],
 ];
 
 test.each(cases)('genDiff', (path1, path2, formatName, expected) => {

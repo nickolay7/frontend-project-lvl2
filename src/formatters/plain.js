49828ca780d7950e2730b5d3ff1185cfd20a48fe
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-17 13:46:38 +0300

    refactor

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,24 +8,27 @@ const currentValue = (item) => {
   return item;
 };
 
-const plain = (data, path = '') => {
-  if (typeof data !== 'object') {
-    return null;
-  }
-  return data.map(({
-    type, key, value, children,
-  }) => {
-    if (type === 'updated') {
-      return `Property '${path}${key}' was updated. From ${currentValue(value.valueBefore)} to ${currentValue(value.valueAfter)}`;
+const plain = (data) => {
+  const mapping = (items, path = '') => {
+    if (typeof items !== 'object') {
+      return null;
     }
-    if (type === 'added') {
-      return `Property '${path}${key}' was added with value: ${currentValue(value)}`;
-    }
-    if (type === 'removed') {
-      return `Property '${path}${key}' was removed`;
-    }
-    return plain(children, `${path}${key}.`);
-  }).filter((el) => el).join('\n');
+    return items.map(({
+      type, key, value, children,
+    }) => {
+      if (type === 'updated') {
+        return `Property '${path}${key}' was updated. From ${currentValue(value.valueBefore)} to ${currentValue(value.valueAfter)}`;
+      }
+      if (type === 'added') {
+        return `Property '${path}${key}' was added with value: ${currentValue(value)}`;
+      }
+      if (type === 'removed') {
+        return `Property '${path}${key}' was removed`;
+      }
+      return mapping(children, `${path}${key}.`);
+    }).filter((el) => el).join('\n');
+  };
+  return mapping(data);
 };
 
 export default plain;

64f6d19b5e75e523a351b12b974ff11c8e70f685
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-18 22:42:16 +0300

    refactor

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,39 +2,41 @@ import _ from 'lodash';
 
 const getIndents = (depth) => {
   const replacer = ' ';
-  const spacesCount = 2;
-  const indentSize = depth * spacesCount;
+  const spacesCount = 4;
+  const indentForSign = 2;
+  const depthStep = 1;
+  const indentSize = depth * spacesCount - indentForSign;
   const currentIndent = replacer.repeat(indentSize);
-  const bracketIndent = replacer.repeat(indentSize - spacesCount);
-  return [spacesCount, currentIndent, bracketIndent];
+  const bracketIndent = replacer.repeat(indentSize - indentForSign);
+  return [depthStep, currentIndent, bracketIndent];
 };
 const stylish = (data) => {
   const iter = (node, depth) => {
     if (!_.isObject(node)) {
       return node;
     }
-    const [, currentIndent, bracketIndent] = getIndents(depth);
+    const [depthStep, currentIndent, bracketIndent] = getIndents(depth);
     const build = ({
       type, key, value, children,
     }) => {
       switch (type) {
         case 'added':
-          return `${currentIndent}+ ${key}: ${iter(value, depth + 1)}`;
+          return `${currentIndent}+ ${key}: ${iter(value, depth + depthStep)}`;
         case 'removed':
-          return `${currentIndent}- ${key}: ${iter(value, depth + 1)}`;
+          return `${currentIndent}- ${key}: ${iter(value, depth + depthStep)}`;
         case 'updated':
-          return [`${currentIndent}- ${key}: ${iter(value.valueBefore, depth + 1)}`,
-            `${currentIndent}+ ${key}: ${iter(value.valueAfter, depth + 1)}`];
+          return [`${currentIndent}- ${key}: ${iter(value.valueBefore, depth + depthStep)}`,
+            `${currentIndent}+ ${key}: ${iter(value.valueAfter, depth + depthStep)}`];
         case 'unchanged':
-          return `${currentIndent}  ${key}: ${iter(value, depth + 1)}`;
+          return `${currentIndent}  ${key}: ${iter(value, depth + depthStep)}`;
         case 'nested':
-          return `${currentIndent}  ${key}: ${iter(children, depth + 1)}`;
+          return `${currentIndent}  ${key}: ${iter(children, depth + depthStep)}`;
         default:
           return new Error(type);
       }
     };
     const lines = !Array.isArray(node)
-      ? _.entries(node).map(([key, value]) => ` ${currentIndent} ${key}: ${iter(value, depth + 1)}`)
+      ? _.entries(node).map(([key, value]) => ` ${currentIndent} ${key}: ${iter(value, depth + depthStep)}`)
       : node.flatMap(build);
     return [
       '{',

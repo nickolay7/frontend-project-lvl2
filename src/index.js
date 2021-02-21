93060378acc636f738c21815c3ce0a4c60abd771
Nickolay Semutkin <shkelal50@gmail.com>
2021-02-21 10:21:38 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import getDiffData from './getDiffData.js';
 export default (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = fs.readFileSync(filepath1, 'utf-8');
   const data2 = fs.readFileSync(filepath2, 'utf-8');
-  const getExtension = (fp) => path.extname(fp).slice(1);
-  const content1 = parse(getExtension(filepath1), data1);
-  const content2 = parse(getExtension(filepath2), data2);
+  const getFormatName = (fp) => path.extname(fp).slice(1);
+  const content1 = parse(getFormatName(filepath1), data1);
+  const content2 = parse(getFormatName(filepath2), data2);
   const diff = getDiffData(content1, content2);
   return format(diff, formatName);
 };
